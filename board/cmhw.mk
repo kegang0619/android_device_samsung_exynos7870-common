f1ad254f1d9c7672be9eabf494bea2abb93fd4df
Yukick <mohiuddinkafre@gmail.com>
2018-02-08 07:45:17 +0000

    cmhw:amoled enhancements from hw/samy

--- board/cmhw.mk
+++ board/cmhw.mk
@@ -15,5 +15,4 @@
 #
 
 # Samsung Hardware
-#BOARD_HARDWARE_CLASS := hardware/samsung/cmhw
-BOARD_HARDWARE_CLASS := device/samsung/exynos7870-common/cmhw
+BOARD_HARDWARE_CLASS := hardware/samsung/cmhw

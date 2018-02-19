15fd7305bb9e2011fde4fce07eced506b6b15b67
Lukas0610 <mail@lukasberger.at>
2018-02-19 16:45:52 +0100

    zero: prevent OMX's storeMetaDataInBuffers-capability from being enabled
    This fixes the damaged hardware-accelerated OMX-decoders, but not the underlying bug (invalid buffer-handles being passed around)
    
    For now, this is the best solution I can imagine.
    
    Change-Id: I459472110b1e8eff826d15bf77b199ff43f2e213

--- product/media.mk
+++ product/media.mk
@@ -29,4 +29,9 @@ PRODUCT_PACKAGES += \
     libion \
     libfimg \
     libstagefright_shim \
-    libui_shim
+    libui_shim \
+    libExynosOMX_shim
+
+# Overriden service definition
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/init/android.hardware.media.omx@1.0-service.rc:system/vendor/etc/init/android.hardware.media.omx@1.0-service.rc
